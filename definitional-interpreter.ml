e626ea515c1f812ce65b46d61032a6b63929a44d
Sumit Ghosh <sumit.ghosh32@gmail.com>
2018-02-12 22:25:22 +0530

    Complete defining types and eval function

--- definitional-interpreter.ml
+++ definitional-interpreter.ml
@@ -1,6 +1,5 @@
-(* 
 type exp = 
-           IntConst of int
+         | IntConst of int
          | Abs of exp
          | Identifier of string
          | Add of exp * exp
@@ -15,118 +14,51 @@ type exp =
          | Implies of exp * exp
          | Eql of exp * exp
          | Grt of exp * exp
-         | Lst of exp * exp
-         | Gre of exp * exp
-         | Lse of exp * exp
-         | Tup of int * exp list
+         | Ls of exp * exp
+         | GrtEql of exp * exp
+         | LsEql of exp * exp
+         | Tuple of int * exp list
          | Proj of int * exp
-;;
-*)
-
-(* 
-type intexp = 
-           IntConst of int
-         | Abs of intexp
-         | Add of intexp * intexp
-         | Sub of intexp * intexp
-         | Mul of intexp * intexp
-         | Div of intexp * intexp
-         | Exp of intexp * intexp
-         | Grt of intexp * intexp
-         | Lst of intexp * intexp
-         | Gre of intexp * intexp
-         | Lse of intexp * intexp
-;;
-
-type boolexp = 
-         | BoolConst of bool
-         | Not of boolexp
-         | And of boolexp * boolexp
-         | Or of boolexp * boolexp
-         | Implies of boolexp * boolexp
-;;
-
-type exp = 
-          IntExp of intexp
-        | BoolExp of boolexp 
-        | TupleExp of int * exp list
-        | Identifier of string
-        | Proj of int * exp
-        | Eql of exp * exp
 ;;
 
 type answer = 
-          IntAns of int
+        | IntAns of int
         | BoolAns of bool
         | TupleAns of int * answer list
 ;;
 
 let rec map f l = match l with
-                      [] -> []
+                    | [] -> []
                     | x::xs -> (f x)::(map f xs)
 ;;
 
+let intof e = match e with IntAns i -> i ;;
+let boolof b = match b with BoolAns b -> b ;;
+let rec listof l = match b with
+                    | [] -> []
+                    | (IntAns x)::xs -> x::(listof xs)
+;;
+
+
 let rec eval rho exp = match exp with
-                          IntExp ie -> inteval rho exp
-                        | BoolExp be -> booleval rho exp
-                        | TupleExp (n, l) -> TupleAns (n, map (eval rho) l)
+                        | IntConst n  -> IntAns n
+                        | Abs e -> IntAns (abs (intof (eval rho e)))
                         | Identifier s -> eval rho (rho s)
-                        | Proj (i, l) -> eval rho (List.nth l i)
-;;
-
-let rec inteval rho intexp = match intexp with
-                          IntConst n  -> IntAns n
-                        | Abs e -> abs (inteval rho e)
-                        | Identifier s -> eval rho (rho s)
-                        | Add (e1, e2) -> (eval rho e1) + (eval rho e2)
-                        | Sub (e1, e2) -> (eval rho e1) + (eval rho e2)
-                        | Mul (e1, e2) -> (eval rho e1) * (eval rho e2)
-                        | Div (e1, e2) -> (eval rho e1) / (eval rho e2)
-                        | Exp (e1, e2) -> int_of_float (float_of_int (eval rho e1) ** float_of_int (eval rho e2))
-                        | BoolConst b -> b
-                        | Not b -> not b
-                        | And (e1, e2) -> (eval rho e1) && (eval rho e2)
-                        | Or (e1, e2) -> (eval rho e1) && (eval rho e2)
-                        | Implies (e1, e2) -> (not (eval rho e1)) || (eval rho e2)
-                        | Eql (e1, e2) -> (eval rho e1) = (eval rho r2)
-                        | Grt (e1, e2) -> (eval rho e1) > (eval rho e2)
-                        | Gre (e1, e2) -> (eval rho e1) >= (eval rho e2)
-                        | Lst (e1, e2) -> (eval rho e1) < (eval rho e2)
-                        | Lse (e1, e2) -> (eval rho e1) <= (eval rho e2)
-                        | Tup (n, l) -> l
-                        | Proj (n, e) -> List.nth l n
-;;
-*)
-
-
-type intexp = 
-              IntConst of int
-            | Plus of intexp * intexp
-;;
-
-type boolexp = 
-              BoolConst of bool
-            | And of boolexp * boolexp
-;;
-
-type exp = 
-              IntExp of intexp
-            | BoolExp of boolexp
-;;
-
-type answer = IntAns of int | BoolAns of bool ;;
-
-let rec inteval e = match e with
-                        | IntConst n -> IntAns n
-                        | Plus (e1, e2) -> IntAns ((match (inteval e1) with IntAns a1 -> a1) + (match (inteval e2) with IntAns a2 -> a2))
-;;
-
-let rec booleval e = match e with
+                        | Add (e1, e2) -> IntAns ((intof (eval rho e1)) + (intof (eval rho e2)))
+                        | Sub (e1, e2) -> IntAns ((intof (eval rho e1)) - (intof (eval rho e2)))
+                        | Mul (e1, e2) -> IntAns ((intof (eval rho e1)) * (intof (eval rho e2)))
+                        | Div (e1, e2) -> IntAns ((intof (eval rho e1)) / (intof (eval rho e2)))
+                        | Exp (e1, e2) -> IntAns (int_of_float ((float_of_int (intof (eval rho e1))) ** float_of_int (intof (eval rho e2))))
                         | BoolConst b -> BoolAns b
-                        | And (e1, e2) -> BoolAns((match (booleval e1) with BoolAns b1 -> b1) && (match (booleval e2) with BoolAns b2 -> b2))
-;;
-
-let rec eval e = match e with
-                        | IntExp e -> inteval e
-                        | BoolExp b -> booleval b
+                        | Not b -> BoolAns (not (boolof (eval rho b)))
+                        | And (e1, e2) -> BoolAns ((boolof (eval rho e1)) && (boolof (eval rho e2)))
+                        | Or (e1, e2) -> BoolAns ((boolof (eval rho e1)) || (boolof (eval rho e2)))
+                        | Implies (e1, e2) -> BoolAns ((not (boolof (eval rho e1))) || (boolof (eval rho e2)))
+                        | Eql (e1, e2) -> BoolAns ((eval rho e1) = (eval rho e2))
+                        | Grt (e1, e2) -> BoolAns ((intof (eval rho e1)) > (intof (eval rho e2)))
+                        | Ls (e1, e2) -> BoolAns ((intof (eval rho e1)) < (intof (eval rho e2)))
+                        | GrtEql (e1, e2) -> BoolAns ((intof (eval rho e1)) >= (intof (eval rho e2)))
+                        | LsEql (e1, e2) -> BoolAns ((intof (eval rho e1)) <= (intof (eval rho e2)))
+                        | Tuple (n, l) -> TupleAns (n, map (eval rho) l)
+                        | Proj (i, Tuple (n, l)) -> eval rho (List.nth l n)
 ;;
